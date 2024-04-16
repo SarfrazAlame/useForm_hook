13204a0b14919eab5e681de212cc14ed975b4a08
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-04-16 18:56:17 +0530

    sadklhf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,71 @@
 "use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { resolve } from "path";
 import React from "react";
 
 import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
 
-type FormFields = {
-  email: string;
-  password: string;
-};
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+type FormFields = z.infer<typeof schema>;
+
+// type FormFields = {
+//   email: string;
+//   password: string;
+// };
 
 const page = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<FormFields>();
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>({
+    defaultValues: {
+      email: "test@test.com",
+      password: "123456",
+    },
+    resolver: zodResolver(schema),
+  });
 
-  const onSubmit: SubmitHandler<FormFields> = (data) => {
-    console.log(data);
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      throw new Error();
+      console.log(data);
+    } catch (error) {
+      setError("email", {
+        message: "This email is already taken",
+      });
+    }
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
       <input
-        {...register("email", {
-          required: "Email is required",
-          validate: (value) => value.includes("@"),
-        })}
+        {...register("email")}
         type="text"
         placeholder="Email"
         className="border h-20"
       />
       {errors.email && <div>{errors.email.message}</div>}
       <input
-        {...register("password", {
-          required: "password is required",
-          minLength: 6,
-        })}
+        {...register("password")}
         type="password"
         placeholder="Password"
         className="border h-20"
       />
       {errors.password && <div>{errors.password.message}</div>}
-      <button type="submit" className="bg-blue-600 h-12">
-        {" "}
-        submit
+      <button
+        disabled={isSubmitting}
+        type="submit"
+        className="bg-blue-600 h-12"
+      >
+        {isSubmitting ? "Loading..." : "Submit"}
       </button>
     </form>
   );
